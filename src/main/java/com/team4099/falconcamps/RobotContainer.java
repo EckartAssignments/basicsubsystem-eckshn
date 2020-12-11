f4bf3c13cb111ecff394f86679f52cee090a9b15
Eckart Schneider <eckshn@gmail.com>
2020-12-11 14:49:52 -0500

    ignore

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -9,10 +9,12 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
 
     private final IntakeSubsystem intake = new IntakeSubsystem();
+    //ignore xbox thing
     XboxController controller = new XboxController(0);
 
     public RobotContainer() {
         intake.setDefaultCommand(new IntakeDefaultCommand(intake));
+        //ignore line below
         new Trigger(controller::getAButton).whenActive(new IntakeCommand(intake));
     }
 }
